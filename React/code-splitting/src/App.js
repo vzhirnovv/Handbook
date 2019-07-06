123525f3881743c3d9e58c510a6a2713916be01b
Vasily Zhirnov <vass.zhirnov@gmail.com>
2019-07-06 14:23:44 +0300

    Add React Code Splitting

--- React/code-splitting/src/App.js
+++ React/code-splitting/src/App.js
@@ -1,23 +1,44 @@
-import React, { Suspense } from 'react';
+import React, { Component, Suspense } from 'react';
+// import LazyComponent from './lazy.jsx';
 import './App.css';
 
 const LazyComponent = React.lazy(() => import('./lazy.jsx'))
 
-function App() {
-  
-  {/* dynamic import example */}
-  import("./math.js").then(Math => {
-    console.log(Math.add(15, 25))
-  });
 
-  return (
-    <div className="App">
-      {/* Suspense example */}
-      <Suspense fallback={<div>Loading...</div>}>
-	<LazyComponent />       
-      </Suspense>	
-    </div>
-  );
+class App extends Component {
+   
+  constructor(props) {
+    super(props)
+    this.state = {
+      showLazyComponent: false
+    };
+  }
+
+  togglePostList = () => {
+    this.setState({
+      showLazyComponent: !this.state.showLazyComponent
+    })
+  }
+
+  render() { 
+    {/* dynamic import example */}
+    import("./math.js").then(Math => {
+      console.log(Math.add(15, 25))
+    });
+
+    return (
+      <div className="App">
+	<button onClick={this.togglePostList}>Show post list!</button>
+	{this.state.showLazyComponent && 
+	    (
+	      <Suspense fallback={<div>Loading...</div>}>
+		<LazyComponent />       
+	      </Suspense>
+	    )
+	}	
+      </div>
+    );
+  }
 }
 
 export default App;
--- React/code-splitting/src/lazy.jsx
+++ React/code-splitting/src/lazy.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const LazyComponent = props => {
-  return (
-    <div>
-      <h1>Lazy component loaded!</h1>
-    </div>
-  )
+class LazyComponent extends Component { 
+  fetchData = async () => {
+    await new Promise(resolve => setTimeout(resolve, 3000))
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const result = await response.json()
+    console.log(result)
+    this.setState({
+      data: result
+    })
+    console.log('data acquired!')
+}
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: [] 
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData() 
+    console.log(this.state.data) 
+  }
+
+  render() {
+    return (
+      <div>
+	<h1>Lazy component loaded!</h1>
+	<ul>
+	  {this.state.data.map((item, key) => {
+	    return (
+	      <li key={key}>
+		<h3>{item.userId}</h3>
+		<h4>{item.title}</h4>
+		<p>{item.body}</p>
+	      </li>
+	    )
+	  })}
+	</ul>
+      </div>
+    )
+  }
 }
 
 export default LazyComponent
